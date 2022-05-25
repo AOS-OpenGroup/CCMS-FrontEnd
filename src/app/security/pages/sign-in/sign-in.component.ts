33ea5b4e571a06a79637648910e0a4e7c635ab02
jrenzoo <u202118568@upc.edu.pe>
2022-05-25 11:57:27 -0500

    feat(auth): implemented authentication services for login

---
+++ src/app/security/pages/sign-in/sign-in.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../../services/auth.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.css']
+})
+export class SignInComponent{
+  signInForm: FormGroup;
+
+
+  hide = true;
+  // email = new FormControl('', [Validators.required, Validators.email]);
+
+  // getErrorMessage() {
+  //   if (this.email.hasError('required')) {
+  //     return 'You must enter a value';
+  //   }
+
+  //   return this.email.hasError('email') ? 'Not a valid email' : '';
+  // }
+  constructor(public builder: FormBuilder, public authService: AuthService, public router: Router) {
+    this.signInForm = this.builder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+   }
+
+   get email() { return this.signInForm.controls['email'];}
+
+   get password() { return this.signInForm.controls['password'];}
+
+   signIn() {
+    this.authService.signIn(this.signInForm.value).subscribe((response: any) => {
+      this.authService.setToken(JSON.stringify(response.accessToken));
+      this.authService.setCurrentUser(JSON.stringify(response.user));
+      this.signInForm.reset();
+      console.log(`accessToken: ${this.authService.getToken()}`);
+      this.router.navigate(['/home']).then();
+    })
+  }
+
+  cancelSignIn() {
+    console.log('Cancelled');
+    this.router.navigate(['/']).then();
+  }
+}
