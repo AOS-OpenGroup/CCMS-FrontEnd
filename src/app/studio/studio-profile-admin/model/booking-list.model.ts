c4d6493a30d1d35c0e5bc8b790f65a91a4b6751c
Rojoxt <64550279+Rojoxt@users.noreply.github.com>
2022-06-10 20:35:13 -0500

    feat: link to json-server with booking

--- src/app/studio/studio-profile-admin/model/booking-list.model.ts
+++
@@ -1,6 +0,0 @@
-export interface BookingListModel{
-  name: string;
-  id: number;
-  weight: number;
-  symbol: string;
-}
---
+++ src/app/studio/studio-profile-admin/model/booking.ts
@@ -0,0 +1,8 @@
+export interface Booking{
+  id: number;
+  studioId :number;
+  customer:string;
+  date: string;
+  time: string;
+  status: string;
+}
