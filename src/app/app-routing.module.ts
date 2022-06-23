24dc8c9bfb11f0e64501e05753924a427c351cbd
Raul Ventura <u201714049@upc.edu.pe>
2022-06-23 16:09:37 -0500

    feat : Implemented Html and Css components of Payment method.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,7 @@ import {BookingsStudioProfileComponent } from './studio/studio_profile/pages/boo
 import {UpcomingComponent } from './studio/studio-profile-admin/pages/home/home.component';
 import{BookingListComponent} from "./studio/studio-profile-admin/pages/booking-list/booking-list.component";
 import {BookTheStudioComponent} from "./studio/studio_profile/pages/book-the-studio/book-the-studio.component";
+import {PaymentComponent} from "./studio/studio_profile/pages/payment/payment.component";
 
 const routes: Routes = [
   {path: 'studio-profile/bookings', component: BookingsStudioProfileComponent},
@@ -32,7 +33,8 @@ const routes: Routes = [
   {path: 'home/near-you', component: StudiosComponent},
   {path:'admin/home', component: UpcomingComponent},
   {path: 'admin/home/bookings',component:BookingListComponent},
-  {path: 'studio-profile/book-the-studio',component:BookTheStudioComponent}
+  {path: 'studio-profile/book-the-studio',component:BookTheStudioComponent},
+  {path: 'studio-profile/book-the-studio/payment',component:PaymentComponent}
 
 ];
 
