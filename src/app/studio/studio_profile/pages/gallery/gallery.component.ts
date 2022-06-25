852ce4c78030016c6c6c8ea271904ec1cbbe4339
jrenzoo <u202118568@upc.edu.pe>
2022-06-25 18:21:51 -0500

    Feat: updated gallery elements

--- src/app/studio/studio_profile/pages/gallery/gallery.component.ts
+++ src/app/studio/studio_profile/pages/gallery/gallery.component.ts
@@ -19,9 +19,9 @@ export class GalleryComponent implements OnInit {
   constructor(private route: ActivatedRoute, private studiosService: StudioProfileService) { }
   tiles: Tile[] = [
     {cols: 3, rows: 1, url: 'https://i.pinimg.com/originals/37/db/8c/37db8c2683d79b42e949fbc17c2f1459.jpg',color: 'lightblue'},
-    {cols: 1, rows: 2, url: 'https://i.pinimg.com/originals/37/db/8c/37db8c2683d79b42e949fbc17c2f1459.jpg',color: 'lightgreen'},
-    {cols: 1, rows: 1, url: 'https://i.pinimg.com/originals/37/db/8c/37db8c2683d79b42e949fbc17c2f1459.jpg',color: 'lightpink'},
-    {cols: 2, rows: 1, url: 'https://i.pinimg.com/originals/37/db/8c/37db8c2683d79b42e949fbc17c2f1459.jpg',color: '#DDBDF1'},
+    {cols: 1, rows: 2, url: 'https://images.pexels.com/photos/2049411/pexels-photo-2049411.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',color: 'lightgreen'},
+    {cols: 1, rows: 1, url: 'https://images.pexels.com/photos/1751731/pexels-photo-1751731.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',color: 'lightpink'},
+    {cols: 2, rows: 1, url: 'https://images.pexels.com/photos/7586659/pexels-photo-7586659.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',color: '#DDBDF1'},
   ];
 
   ngOnInit(): void {
