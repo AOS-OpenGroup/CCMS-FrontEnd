59687ffa5d65d54850080496bc86be234d170512
jrenzoo <u202118568@upc.edu.pe>
2022-06-25 17:29:45 -0500

    feat: implemented studio interface for gallery

--- src/app/studio/studio_profile/pages/gallery/gallery.component.ts
+++ src/app/studio/studio_profile/pages/gallery/gallery.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Studio } from 'src/app/bookings/model/studio';
+import { StudioProfileService } from '../../services/studio-profile.service';
 
 export interface Tile {
   color: string;
@@ -12,15 +15,25 @@ export interface Tile {
   styleUrls: ['./gallery.component.css']
 })
 export class GalleryComponent implements OnInit {
-
-  constructor() { }
+  studio!: Studio;
+  constructor(private route: ActivatedRoute, private studiosService: StudioProfileService) { }
   tiles: Tile[] = [
     {cols: 3, rows: 1, url: 'https://i.pinimg.com/originals/37/db/8c/37db8c2683d79b42e949fbc17c2f1459.jpg',color: 'lightblue'},
     {cols: 1, rows: 2, url: 'https://i.pinimg.com/originals/37/db/8c/37db8c2683d79b42e949fbc17c2f1459.jpg',color: 'lightgreen'},
     {cols: 1, rows: 1, url: 'https://i.pinimg.com/originals/37/db/8c/37db8c2683d79b42e949fbc17c2f1459.jpg',color: 'lightpink'},
     {cols: 2, rows: 1, url: 'https://i.pinimg.com/originals/37/db/8c/37db8c2683d79b42e949fbc17c2f1459.jpg',color: '#DDBDF1'},
   ];
+
   ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const studioId = Number(routeParams.get('studioId'));
+
+    this.getStudio(studioId);
+  }
+  getStudio(studioId: number) : void{
+    this.studiosService.getById(studioId).subscribe((response: any) => {
+      this.studio = response;
+    });
   }
 
 }
